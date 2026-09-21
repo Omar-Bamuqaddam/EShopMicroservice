93ab7173e654ccbefc171e38a977e1f2bd92f3a0
Omar-Bamuqaddam <53944562+Omar-Bamuqaddam@users.noreply.github.com>
2026-09-21 14:40:29 +0300

    .46

--- Eshop-Microservice/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ Eshop-Microservice/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,10 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
 {
     public class CreateProductHandler
     {
